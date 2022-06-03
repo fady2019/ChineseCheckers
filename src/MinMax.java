4e4c9cefbc2f277f8b5036c3873181a45a7aa4c4
Fady Emad <fadyemadfawzygoher258456@gmail.com>
2022-06-03 16:17:40 +0200

    minMax function is updated

--- src/MinMax.java
+++ src/MinMax.java
@@ -1,8 +1,22 @@
 import java.util.ArrayList;
 import java.util.List;
 
+class MoveInfo{
+    public int srcCell = -1;
+    public int destCell = -1;
+    public int val;
+}
+
 public class MinMax {
-    public static void minMax(Common.Player player, int levelNum, Board CCBoard){
+    private static MoveInfo utilityFun(Board board){
+        return null;
+    }
+
+    public static MoveInfo minMax(Common.Player player, int levelNum, Board CCBoard, boolean maximizing){
+        if(levelNum == 0){
+            return utilityFun(CCBoard);
+        }
+
         levelNum--;
 
         ArrayList<Integer> marblesCells;
@@ -12,6 +26,9 @@ public class MinMax {
         }else{
             marblesCells = CCBoard.redMarbleCells;
         }
+
+        MoveInfo bestMove = new MoveInfo();
+        bestMove.val = maximizing? Integer.MIN_VALUE : Integer.MAX_VALUE;
 
         for(int srcCell: marblesCells){
             List<Integer> possibleMoves = new ArrayList<>();
@@ -28,7 +45,18 @@ public class MinMax {
             for(int destCell: possibleMoves){
                 Board childBoard = CCBoard.copy();
                 Move.moveMarble(player, childBoard, srcCell, destCell);
+                Common.Player nextPlayer = player == Common.Player.HUMAN? Common.Player.PC : Common.Player.HUMAN;
+                
+                MoveInfo moveInfo = minMax(nextPlayer, levelNum-1, childBoard, !maximizing);
+                moveInfo.srcCell = srcCell;
+                moveInfo.destCell = destCell;
+                
+                if((maximizing && moveInfo.val > bestMove.val) || (!maximizing && moveInfo.val < bestMove.val)){
+                    bestMove = moveInfo;
+                }
             }
         }
+
+        return bestMove;
     }
 }
